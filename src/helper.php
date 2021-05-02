12d4d0a30fd898969410449fd982ab36fda3a4c7
kain <zhangtqx@vip.qq.com>
2021-05-02 19:48:11 +0800

    update

--- src/helper.php
+++ src/helper.php
@@ -69,20 +69,3 @@ if (!function_exists('stringy')) {
         return Stringy::create($str, $encoding);
     }
 }
-
-if (!function_exists('get_client_ip')) {
-    /**
-     * 获取客户端 IP
-     * @return string
-     */
-    function get_client_ip(): string
-    {
-        if (!empty($_SERVER['REMOTE_ADDR'])) {
-            $ip = $_SERVER['REMOTE_ADDR'];
-        } else {
-            // for php-cli(phpunit etc.)
-            $ip = defined('PHPUNIT_RUNNING') ? '127.0.0.1' : gethostbyname(gethostname());
-        }
-        return filter_var($ip, FILTER_VALIDATE_IP) ?: '127.0.0.1';
-    }
-}
